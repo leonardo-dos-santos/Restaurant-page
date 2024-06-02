25a089b549dbbd0a237ac742e40dc682e4d8c06c
Leonardo <129320373+leonardo-dos-santos@users.noreply.github.com>
2024-06-02 10:44:22 -0300

    mudanças

--- bundle.js
+++ bundle.js
@@ -10,13 +10,23 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/homepage.js":
-/*!*************************!*\
-  !*** ./src/homepage.js ***!
-  \*************************/
+/***/ "./src/contact.js":
+/*!************************!*\
+  !*** ./src/contact.js ***!
+  \************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction loadHomePage() {\r\n  const content = document.getElementById('content');\r\n\r\n  const headline = document.createElement('h1');\r\n  headline.textContent = \"Welcome to our Restaurant!\";\r\n\r\n  const image = document.createElement('img');\r\n  image.src = './restaurant-img.jpg'\r\n  image.alt = 'Restaurant Image';\r\n\r\n  const description = document.createElement('p');\r\n  description.textContent = \"We serve the best food in town. Come and enjoy our delicious meals!\";\r\n\r\n  content.appendChild(headline);\r\n  content.appendChild(image);\r\n  content.appendChild(description);\r\n  \r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (loadHomePage);\n\n//# sourceURL=webpack:///./src/homepage.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction loadContact() {\r\n  const content = document.getElementById('content');\r\n  content.textContent = ''; // Clear previous content\r\n\r\n  const headline = document.createElement('h1');\r\n  headline.textContent = \"Contact Us\";\r\n\r\n  const contactInfo = document.createElement('p');\r\n  contactInfo.textContent = \"Email: contact@restaurant.com\\nPhone: 123-456-7890\";\r\n\r\n  content.appendChild(headline);\r\n  content.appendChild(contactInfo);\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (loadContact);\r\n\n\n//# sourceURL=webpack://restaurant/./src/contact.js?");
+
+/***/ }),
+
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction loadHome() {\r\n  const content = document.getElementById('content');\r\n  content.textContent = '';\r\n\r\n  const headline = document.createElement('h1');\r\n  headline.textContent = 'Welcome to our Restaurant!';\r\n\r\n  const image = document.createElement('img');\r\n  image.src = './img/restaurant-img.jpg';\r\n  image.alt = 'Restaurant Image';\r\n\r\n  const description = document.createElement('p');\r\n  description.textContent = 'We serve the best food in town. Come and enjoy our delicious meals!';\r\n\r\n  content.appendChild(headline);\r\n  content.appendChild(image);\r\n  content.appendChild(description);\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (loadHome);\n\n//# sourceURL=webpack://restaurant/./src/home.js?");
 
 /***/ }),
 
@@ -26,7 +36,17 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _homepage__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./homepage */ \"./src/homepage.js\");\n\r\n\r\nconsole.log('Webpack is working!');\r\n(0,_homepage__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\r\n\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home */ \"./src/home.js\");\n/* harmony import */ var _menu__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./menu */ \"./src/menu.js\");\n/* harmony import */ var _contact__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./contact */ \"./src/contact.js\");\n\r\n\r\n\r\n\r\nfunction init() {\r\n  (0,_home__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(); // Load home by default\r\n\r\n  const homeButton = document.querySelector('button:nth-child(1)');\r\n  const menuButton = document.querySelector('button:nth-child(2)');\r\n  const contactButton = document.querySelector('button:nth-child(3)');\r\n\r\n  homeButton.addEventListener('click', _home__WEBPACK_IMPORTED_MODULE_0__[\"default\"]);\r\n  menuButton.addEventListener('click', _menu__WEBPACK_IMPORTED_MODULE_1__[\"default\"]);\r\n  contactButton.addEventListener('click',_contact__WEBPACK_IMPORTED_MODULE_2__[\"default\"]);\r\n}\r\n\r\ndocument.addEventListener('DOMContentLoaded', init);\r\n\n\n//# sourceURL=webpack://restaurant/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/menu.js":
+/*!*********************!*\
+  !*** ./src/menu.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction loadMenu() {\r\n  const content = document.getElementById('content');\r\n  content.textContent = ''; // Clear previous content\r\n\r\n  const headline = document.createElement('h1');\r\n  headline.textContent = \"Our Menu\";\r\n\r\n  const menuList = document.createElement('ul');\r\n  const items = ['Pizza', 'Pasta', 'Salad'];\r\n  items.forEach(item => {\r\n      const listItem = document.createElement('li');\r\n      listItem.textContent = item;\r\n      menuList.appendChild(listItem);\r\n  });\r\n\r\n  content.appendChild(headline);\r\n  content.appendChild(menuList);\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (loadMenu);\n\n//# sourceURL=webpack://restaurant/./src/menu.js?");
 
 /***/ })
 
